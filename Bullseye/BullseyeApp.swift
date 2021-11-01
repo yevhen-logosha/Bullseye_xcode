f9bb47e7bff9d19a50e0463d6da6c4114cd89e58
Yevhen Lohosha <evlogosha@gmail.com>
2021-11-01 11:52:49 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Yevhen Lohosha on 01/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
