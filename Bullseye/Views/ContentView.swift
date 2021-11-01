18fd57586c0b6c548188c413d382bf5f5871e534
Yevhen Lohosha <evlogosha@gmail.com>
2021-11-01 20:17:31 +0100

    style: colours. button color

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,35 +14,51 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CANN TO")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text(String(game.target))
-                .kerning(-1)
-                .fontWeight(.black)
-                .font(.largeTitle)
-            HStack {
-                Text("1")
-                    .fontWeight(.bold)
-                Slider(value: self.$sliderValue, in: 1.0...100)
-                Text("100")
-                    .fontWeight(.bold)
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("🎯🎯🎯\nPut Bullseye as close as you can to".uppercased())
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text(String(game.target))
+                    .kerning(-1)
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                HStack {
+                    Text("1")
+                        .fontWeight(.bold)
+                    Slider(value: self.$sliderValue, in: 1.0...100)
+                    Text("100")
+                        .fontWeight(.bold)
+                }
+                Button(action: {
+                    print("Hello Swiftui")
+                    self.alertIsVisible = true
+                }) {
+                    Text("Hit me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
+                .alert(isPresented: $alertIsVisible, content: {
+                    let roundedValue = Int(self.sliderValue.rounded())
+                
+                    return Alert(title: Text("Hello There!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
+                })
+                .padding(20.0)
+                .foregroundColor(Color.white)
+                .background(
+                    ZStack {
+                        Color("ButtonColor")
+                        LinearGradient(colors: [Color.white.opacity(0.3), Color.clear], startPoint: .top, endPoint: .bottom)
+                    }
+                )
+                .cornerRadius(21.0)
+                
             }
-            Button(action: {
-                print("Hello Swiftui")
-                self.alertIsVisible = true
-            }) {
-                Text("Hit me")
-            }
-            .alert(isPresented: $alertIsVisible, content: {
-                let roundedValue = Int(self.sliderValue.rounded())
-            
-                return Alert(title: Text("Hello There!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
-            })
         }
     }
 }
